6ebf2fe0e24bd2ef76f860b1d4ac2668743dd0db
Shreyas-99 <shreyaskad2005@gmail.com>
2025-06-10 11:02:10 +0530

    Revert "created onboarding Form,fixed favicon,navbar"
    This reverts commit 38cd15151faf126df3f0bdcc103344221c747fd3.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from '../components/Footer';
-import Navbar from "@/components/ui/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
           {children}
           <Footer/>
       </body>
