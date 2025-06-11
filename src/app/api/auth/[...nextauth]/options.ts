05ec902ba387ea887eff88f625ad7c3f464042b4
Shikhar Shetty <shettyshikhar99@gmail.com>
2025-06-11 16:30:04 +0530

    Prisma+Vercel: Fixed

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,42 +1,43 @@
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from "@/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-export const authOptions: NextAuthOptions = {
+export const authOptions = async (): Promise<NextAuthOptions> => {
+  const { default: prisma } = await import("@/lib/prisma");
+
+  return {
     providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!
-        })
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID!,
+        clientSecret: process.env.GITHUB_SECRET!,
+      }),
     ],
     callbacks: {
-        async jwt({ token, user }){
-            if(user){
-                console.log("JWT callback:", user);
-                token.sub = user.id;
-                token.username = user.username;
-            }
-            return token;
-        },
-        async session({ session, token }){
-            if(token){
-                session.user = {
-                    ...session.user,
-                    id: token.sub,
-                    username: token.username,
-                }
-            }
-            return session;
+      async jwt({ token, user }) {
+        if (user) {
+          token.sub = user.id;
+          token.username = user.username;
         }
+        return token;
+      },
+      async session({ session, token }) {
+        if (token) {
+          session.user = {
+            ...session.user,
+            id: token.sub,
+            username: token.username,
+          };
+        }
+        return session;
+      },
     },
     session: {
-        strategy: "jwt"
+      strategy: "jwt",
     },
     adapter: PrismaAdapter(prisma),
     secret: process.env.NEXTAUTH_SECRET,
     pages: {
-        signIn: "/sign-in"
-    }
-}
-
+      signIn: "/sign-in",
+    },
+  };
+};
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
 import { authOptions } from "./options";
 
-const handler = NextAuth(authOptions)
-export {handler as GET, handler as POST}
+const handler = async (req: Request, res: Response) => {
+  const options = await authOptions();
+  return NextAuth(options)(req, res);  
+};
+
+export { handler as GET, handler as POST };
