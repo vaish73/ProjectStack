871f2067c8b070f8524f4f367db78aa6a1f618f3
Shikhar Shetty <shettyshikhar99@gmail.com>
2025-06-11 12:53:30 +0530

    Prisma+Vercel: Fixed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,19 @@
-import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<PrismaClient['$extends']> | undefined;
+};
+
+let prisma: ReturnType<PrismaClient['$extends']>;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient().$extends(withAccelerate());
+} else {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient().$extends(withAccelerate());
+  }
+  prisma = globalForPrisma.prisma;
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+export default prisma;
